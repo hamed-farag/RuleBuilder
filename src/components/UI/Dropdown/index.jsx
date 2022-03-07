b38fb6b756a1d7819b9ae646a3771ecb57e0b996
Mennatallah Alsayed <mennat.hafez@gmail.com>
2022-03-08 01:22:13 +0200

    fixed logic

--- src/components/UI/Dropdown/index.jsx
+++ src/components/UI/Dropdown/index.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Dropdown as BDropdown } from "react-bootstrap";
 
 import "./styles.scss";
 
 export default function Dropdown(props) {
     const { onChange, options, defaultValue } = props;
-    const [selected, setSelected] = useState(defaultValue);
+    const [selected, setSelected] = useState('');
 
+    useEffect(() => { setSelected(defaultValue) }, [])
     return (
         <BDropdown className="scrps-drpdwn">
             <BDropdown.Toggle variant="primary" id="dropdown-basic">
@@ -15,13 +16,6 @@ export default function Dropdown(props) {
 
             <BDropdown.Menu>
                 {options.map((item) => {
-                    if (selected && item.value === selected) {
-                        return (
-                            <BDropdown.Item value={item.value} selected>
-                                {item.key}
-                            </BDropdown.Item>
-                        );
-                    }
                     return <BDropdown.Item value={item.value} onClick={() => { setSelected(item.value); onChange(item.value); }}>
                         {item.key}
                     </BDropdown.Item>;
